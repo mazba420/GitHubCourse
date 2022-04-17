b655f20fd873eb4c053cf712158cf807c3c6c993
mazba420 <mazba_420@yahoo.com>
2022-04-17 14:00:23 -0400

    first commit

---
+++ src/main/java/Test/HomePageTest.java
@@ -0,0 +1,11 @@
+package Test;
+
+public class HomePageTest {
+    public static void main(String[] args) {
+
+        System.out.println("hello Homepage test");
+
+
+
+    }
+}
---
+++ src/main/java/Test/LoginPageTest.java
@@ -0,0 +1,11 @@
+package Test;
+
+public class LoginPageTest {
+    public static void main(String[] args) {
+
+        System.out.println("hello LP test");
+
+
+
+    }
+}
