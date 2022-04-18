27785311842cba6b0a0486f6988eae839ac7b4d4
mazba420 <mazba_420@yahoo.com>
2022-04-18 10:57:47 -0400

    code is added in logout page

--- src/main/java/Pages/LogoutPage.java
+++ src/main/java/Pages/LogoutPage.java
@@ -3,5 +3,11 @@ package Pages;
 public class LogoutPage {
     public static void main(String[] args) {
         System.out.println("im working here");
+
+        System.out.println("log out page succesfully logout");
+
+
+
+
     }
 }
