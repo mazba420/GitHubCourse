5e4e30b4963942265008e38b4059cd9e760c9fc6
mazba420 <mazba_420@yahoo.com>
2022-04-18 09:44:57 -0400

    cart pages modify

--- src/main/java/Test/CartTest.java
+++ src/main/java/Test/CartTest.java
@@ -4,5 +4,6 @@ public class CartTest {
     public static void main(String[] args) {
 
         System.out.println("Cart test......................");
+        System.out.println("cart features added");
     }
 }
